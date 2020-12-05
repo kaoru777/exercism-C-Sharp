90e7454470ff6905928f9326d0ffd5ceba5925d9
Kaoru Steinboeck <kaoru@steinboeck.dev>
2020-12-05 17:39:33 +0100

    added reverse string solution

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] inputCharArray = input.ToCharArray();
+        string reverseSolution = "";
+        for(var i = inputCharArray.Length - 1; i >= 0; i--)
+        {
+            reverseSolution += inputCharArray[i];
+        }
+        return reverseSolution;
+    }
+}
